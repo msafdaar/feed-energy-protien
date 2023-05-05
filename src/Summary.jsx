af012d48ba4a8cbee8c72191b1021abc424022c3
Safdar <71539665+malta-khan@users.noreply.github.com>
2023-05-05 08:28:55 +0500

    some work on summary

--- src/Summary.jsx
+++ src/Summary.jsx
@@ -1,8 +1,29 @@
 function Summary({formula}) {
-    console.log(formula)
+    //number of items
+    let numberOfItems = formula.length;
+    //total qty
+    let totalQty = formula.reduce((total, item)=>{
+        if(item.qty === undefined){
+            return total;
+        } else {
+            return total+item.qty
+        }
+    }, 0);
+    //total cost
+    let totalCost = formula.reduce((total, item)=>{
+        let qty = item.qty === undefined ? 0 : item.qty;
+        let rate = item.rate === undefined ? 0 : item.rate;
+            return total + (qty*rate);
+    }, 0);
+    //cost per bag (50kg)
+    let costPer50Kg = totalCost/totalQty*50;
+
     return ( <div className=' bg-violet-50 border border-violet-700 shadow-md p-5 rounded-md mt-5'>
     <h2 className='text-2xl mb-2'>Quick Report</h2>
-    <div>Total Cost = {formula[0].qty * formula[0].rate}</div>
+    <div>Item Count = {numberOfItems}</div>
+    <div>Qty Total = {totalQty}</div>
+    <div>Total Cost = {totalCost}</div>
+    <div>Per 50kg = {costPer50Kg}</div>
     </div> );
 }
 
