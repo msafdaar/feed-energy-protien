c27438fa9ffcb2eeb30629a89eca9a4f3c061836
Safdar <71539665+malta-khan@users.noreply.github.com>
2023-05-05 09:40:48 +0500

    more work on summary,fixed some NaN problems

--- src/App.jsx
+++ src/App.jsx
@@ -37,8 +37,8 @@ function App() {
     {formula.map((item, index)=>{
       return <div className='flex gap-1 mb-1' key={index}>
       <Select className='w-2/4' options={options} isClearable={true} onChange={(value) => changeHandler({value, index,inputType: "item"})} data-item-index={index} data-input-type = "item"></Select>
-      <input className='w-1/4 border rounded-md px-2' type='number' min="0" value={item.qty ? item.qty.toString() : 0} onChange={(value) => changeHandler({value: parseFloat(value.target.value), index,inputType: "qty"})} ></input>
-      <input className='w-1/4 border rounded-md px-2' type='number' min="0" value={item.rate ? item.rate.toString() : 0} onChange={(value) => changeHandler({value: parseFloat(value.target.value), index,inputType: "rate"})} ></input>
+      <input className='w-1/4 border rounded-md px-2' type='number' min="0" value={item.qty ? item.qty.toString() : 0} onChange={(value) => changeHandler({value: parseFloat(value.target.value)||0, index,inputType: "qty"})} ></input>
+      <input className='w-1/4 border rounded-md px-2' type='number' min="0" value={item.rate ? item.rate.toString() : 0} onChange={(value) => changeHandler({value: parseFloat(value.target.value)||0, index,inputType: "rate"})} ></input>
       </div>
     })}
     <div className='flex gap-2 mt-2'>
--- src/Summary.jsx
+++ src/Summary.jsx
@@ -1,29 +1,48 @@
 function Summary({formula}) {
-    //number of items
-    let numberOfItems = formula.length;
-    //total qty
-    let totalQty = formula.reduce((total, item)=>{
-        if(item.qty === undefined){
-            return total;
-        } else {
-            return total+item.qty
+    let formulaCopy = formula.map((item)=>{
+        if(item.qty === undefined || item.qty === "" || item.qty === null){
+            item.qty = 0;
         }
-    }, 0);
-    //total cost
-    let totalCost = formula.reduce((total, item)=>{
-        let qty = item.qty === undefined ? 0 : item.qty;
-        let rate = item.rate === undefined ? 0 : item.rate;
-            return total + (qty*rate);
-    }, 0);
-    //cost per bag (50kg)
-    let costPer50Kg = totalCost/totalQty*50;
+        if(item.rate === undefined || item.rate === "" || item.rate === null){
+            item.rate = 0;
+        }
+        if(item.item === undefined || item.item === "" || item.item === null){
+            item.item = {};
+        }
+        return item
+    })
+    console.log(formulaCopy)
 
+    //number of items
+    let numberOfItems = formulaCopy.length;
+    //total qty
+    let totalQty = formulaCopy.reduce((total, item)=>{return total+item.qty}, 0);
+    //total cost
+    let totalCost = formulaCopy.reduce((total, item)=>{return total + (item.qty*item.rate)}, 0);
+    //cost per bag (50kg)
+    let costPer50Kg = totalCost/totalQty*50 || 0;
+    //Dry matter
+    let dryMatter = getFieldTotal("DRY MATTER %", formulaCopy);
+    //protien
+    let protien = getFieldTotal("CP", formulaCopy);
+    //energy
+    let energy = getFieldTotal("Energy", formulaCopy);
+
+    function getFieldTotal(fieldString, formula){
+        return formula.reduce((total, current)=>{
+            let multiplier = current.item[fieldString]/1000 || 0;
+            return total + (current.qty*multiplier)
+        }, 0)
+    }
     return ( <div className=' bg-violet-50 border border-violet-700 shadow-md p-5 rounded-md mt-5'>
     <h2 className='text-2xl mb-2'>Quick Report</h2>
     <div>Item Count = {numberOfItems}</div>
-    <div>Qty Total = {totalQty}</div>
-    <div>Total Cost = {totalCost}</div>
-    <div>Per 50kg = {costPer50Kg}</div>
+    <div>Qty Total = {totalQty}Kg</div>
+    <div>Total Cost = {totalCost}Rs</div>
+    <div>Per 50kg = {costPer50Kg}Rs</div>
+    <div>Dry Matter = {dryMatter}%</div>
+    <div>Energy = {energy}</div>
+    <div>Protien = {protien}%</div>
     </div> );
 }
 
