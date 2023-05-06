6b0e6102d8a199218d4bbda237eaff3ec856e26c
Safdar <71539665+malta-khan@users.noreply.github.com>
2023-05-06 06:32:03 +0500

    full selection on input focus, update styles
    •add function to select full input on focus
    •add tailwind classes to hide arrow •buttons on number inputs
    •fixed alignment of input heading labels

--- src/App.jsx
+++ src/App.jsx
@@ -459,25 +459,26 @@ function App() {
     let newState = formula.filter((value, index)=> {
       return index !== target
     })
-    console.log(formula)
-    console.log(newState)
     setFormula(newState);
+  }
+  function focusHandler(event){
+    event.target.select()
   }
   return (
     <div className=' bg-fuchsia-50 border border-fuchsia-700 shadow-md p-5 rounded-md flex gap-5 sm:gap-10 flex-col sm:flex-row'>
     <div className=''>
     <h2 className='text-2xl mb-2'>Formula items</h2>
     <div className='flex gap-1 mb-1'>
-      <div className='w-2/4'>Item</div>
-      <div className='w-1/4'>Qty</div>
-      <div className='w-1/4'>Rate</div>
+      <div className='w-4/6'>Item</div>
+      <div className='w-1/6'>Qty</div>
+      <div className='w-1/6'>Rate</div>
     </div>
     {formula.map((item, index)=>{
       return <div className='flex gap-1 mb-1 items-center' key={index}>
       <Select className='w-4/6' options={options} isClearable={true} value={item.item || 0} onChange={(value) => changeHandler({value, index,inputType: "item"})}></Select>
-      <input className='w-1/6 border h-10 rounded-md px-2 text-right' type='number' min="0" value={item.qty ? item.qty.toString() : 0} onChange={(value) => changeHandler({value: parseFloat(value.target.value)||0, index,inputType: "qty"})} ></input>
-      <input className='w-1/6 border h-10 rounded-md px-2 text-right' type='number' min="0" value={item.rate ? item.rate.toString() : 0} onChange={(value) => changeHandler({value: parseFloat(value.target.value)||0, index,inputType: "rate"})} ></input>
-      <button className='bg-white h-6 w-6 text-xs text-slate-600 font-bold active:ring hover:bg-slate-100 shadow-sm border rounded-full' onClick={()=>removeItem(index)}>x</button>
+      <input className='w-1/6 border h-10 rounded-md px-2 text-right [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none' type='number' min="0" value={item.qty ? item.qty.toString() : 0} onChange={(value) => changeHandler({value: parseFloat(value.target.value)||0, index,inputType: "qty"})} onFocus={focusHandler}></input>
+      <input className='w-1/6 border h-10 rounded-md px-2 text-right [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none' type='number' min="0" value={item.rate ? item.rate.toString() : 0} onChange={(value) => changeHandler({value: parseFloat(value.target.value)||0, index,inputType: "rate"})} onFocus={focusHandler}></input>
+      <button className="bg-white h-6 w-6 text-xs text-slate-600 active:ring hover:bg-slate-100 shadow-sm border rounded-full" onClick={()=>removeItem(index)}>X</button>
       </div>
     })}
     <div className='flex gap-2 mt-2'>
